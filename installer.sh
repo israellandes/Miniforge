e519e2eb5023387d8624460a763d4669d44ff27b
Is-Rael Landes <israel.landes16@gmail.com>
2023-09-10 02:16:41 -0700

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,6 @@
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh
+sha256sum Miniforge3-Linux-aarch64.sh
+chmod +x Miniforge3-Linux-aarch64.sh
+./Miniforge3-Linux-aarch64.sh
+# Exit & Re-enter shell
+conda activate base
